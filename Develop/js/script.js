670d89b046db23be9c5aadd574e608715ea48e2e
Rahil Rahimi <rahilrahimi1362@gmail.com>
2021-09-26 11:54:02 -0400

    modified index.html and js/script.js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,1 +1,57 @@
 $('#currentDay').text(moment().format('dddd, MMMM Do'));
+
+$(document).ready(function () {
+    var textAreas = $('textarea')
+    var hour = moment().hour()
+    for (var i = 0; i < textAreas.length; i++) {
+        var currentTextArea = textAreas[i]
+        var currentId = parseInt(currentTextArea.id)
+        if (hour < currentId) {
+            $(currentTextArea).addClass("future")
+        }
+        else if (hour > currentId) {
+            $(currentTextArea).addClass("past")
+        }
+        else {
+            $(currentTextArea).addClass("present")
+        }
+
+    }
+
+})
+
+
+//( jquery ) make onsavebtn click run a function inside of that function have cosole.log to veryfay that fuction run
+
+type = "text/javascript"
+$(document).ready(function () {
+    $("#Button1").click(function () {
+        $.ajax({
+            type: 'POST',
+            contentType: "application/json;charset=utf-8",
+            url: 'Default2.aspx/InsertMethod',
+            data: "{'username':'" + document.getElementById('txtusername').value + "','password':'" + document.getElementById("txtpassword").value + "'}",
+            async: false,
+            success: function (response) {
+                $('#txtusername').val('');
+                $('#txtpassword').val('');
+                alert("record has been saved in database");
+
+            },
+            error: function () {
+                console.log('there is some error');
+            }
+
+        });
+
+    });
+
+});
+
+
+
+
+
+
+// get value from spesific text box with "this" function 
+// lastly stor that value into lolcalstorage
