62eb8edfa6c9a70f4bf040df91cfb0aeac0f7d14
Rahil Rahimi <rahilrahimi1362@gmail.com>
2021-09-26 21:56:27 -0400

    modified index.html and modified js/script.js

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,4 +1,5 @@
 $('#currentDay').text(moment().format('dddd, MMMM Do'));
+//  time blocks for standard business hours, whether it is in past,present, or future
 
 $(document).ready(function () {
     var textAreas = $('textarea')
@@ -20,15 +21,34 @@ $(document).ready(function () {
 
 })
 
+// stor that value into lolcalstorage
 
-//( jquery ) make onsavebtn click run a function inside of that function have cosole.log to veryfay that fuction run
-
-$( ".saveBtn" ).click(function(e) {
+$(".saveBtn").click(function (e) {
     console.log(e.target);
     console.log($(this).siblings('.textSave').val());
     localStorage.setItem($(this).siblings('.textSave').attr('id'), $(this).siblings('.textSave').val());
 
-    alert( "Handler for .click() called." );
-  });
-// get value from spesific text box with "this" function 
-// lastly stor that value into lolcalstorage
+    alert("Handler for .click() called.");
+    setStyles();
+});
+//( jquery ) make onsavebtn click run a function inside of that function have cosole.log to veryfay that fuction run
+
+function setStyles() {
+    // console.log(localStorage.getItem('17'));
+    $("#9").text(localStorage.getItem('9'));
+    $("#10").text(localStorage.getItem('10'));
+    $("#11").text(localStorage.getItem('11'));
+    $("#12").text(localStorage.getItem('12'));
+    $("#13").text(localStorage.getItem('13'));
+    $("#14").text(localStorage.getItem('14'));
+    $("#15").text(localStorage.getItem('15'));
+    $("#16").text(localStorage.getItem('16'));
+    $("#17").text(localStorage.getItem('17'));
+    $("#18").text(localStorage.getItem('18'));
+    $("#19").text(localStorage.getItem('19'));
+    $("#20").text(localStorage.getItem('20'));
+    $("#21").text(localStorage.getItem('21'));
+    $("#22").text(localStorage.getItem('22'));
+}
+setStyles();
+
